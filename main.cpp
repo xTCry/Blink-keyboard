7ac4cb140c8dbcb39a436847ff218184517f1250
xTCry <wolt6@mail.ru>
2019-07-10 05:35:01 +0300

    old update
    test

--- main.cpp
+++ main.cpp
@@ -6,6 +6,20 @@ int rand(int min, int max) {
 	return rand() % (max - min + 1) + min;
 }
 
+void SetKeyState(bool state, BYTE keyCode) {
+	byte keyState[256];
+
+	GetKeyboardState((PBYTE)&keyState);
+	if ((state && !(keyState[keyCode] & 1)) || (!state && (keyState[keyCode] & 1))) {
+		
+		keybd_event(keyCode, 0x45,
+			KEYEVENTF_EXTENDEDKEY | 0, 0);
+
+		keybd_event(keyCode, 0x45,
+			KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+	}
+}
+
 void ToggleKeyState(BYTE keyCode) {
 	keybd_event(keyCode, 0x45,
 		KEYEVENTF_EXTENDEDKEY | 0, 0);
@@ -16,6 +30,7 @@ void ToggleKeyState(BYTE keyCode) {
 
 void BlinkKeyboard() {
 	int i = 0;
+	bool bBlink = false;
 	
 	while (true) {
 		int r = rand(0, 2);
@@ -24,7 +39,9 @@ void BlinkKeyboard() {
 					(r == 1)? VK_SCROLL:
 							VK_NUMLOCK;
 		
+		// SetKeyState(bBlink, key);
 		ToggleKeyState(key);
+		bBlink = !bBlink;
 		Sleep(80);
 		i++;
 	}
